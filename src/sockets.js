a1d1ccb6126da8f6f979514cafebc255b38d79bf
ALEJANDRO APODACA <alejandroapodaca@Alexs-MacBook-Pro.local>
2023-08-04 03:40:24 -0700

    delete users fixed in realtime

--- src/sockets.js
+++ src/sockets.js
@@ -27,6 +27,9 @@ const getUserByPassword = async (password) => {
 export default (io) => {
   io.on("connection", (socket) => {
     console.log("New user Connected (Flutter)");
+    User.find().then((users) => {
+      io.emit("server:getusers", users);
+    });
 
     socket.on("client:login", async (password) => {
       const user = await getUserByPassword(password);
@@ -70,36 +73,29 @@ export default (io) => {
       }
     });
 
-    socket.on("client:adduser", async (addedUser) => {
-      try {
-        console.log(addedUser);
+    socket.on("client:adduser", async (userData) => {
+      const users = await User.find();
+      // Verificar si la contraseña ya está en uso
+      const isPasswordUsed = users.some(
+        (user) => user.password == userData.password
+      );
 
-        const newUser = new User({
-          position: addedUser.position,
-          municipalityNumber: addedUser.municipalityNumber,
-          lastName: addedUser.lastName,
-          firstName: addedUser.firstName,
-          gender: addedUser.gender,
-          party: addedUser.party,
-          startDate: addedUser.startDate,
-          endDate: addedUser.endDate,
-          memberStatus: addedUser.memberStatus,
-          memberPhoto: addedUser.memberPhoto,
-          password: addedUser.password,
-        });
+      // Verificar si ya existe otro administrador
+      const isAdminExists = users.some(
+        (user) => user.position === "Administrador"
+      );
 
-        await newUser.save();
-
-        const updatedUsers = await User.find();
-
-        io.emit("server:adduser", updatedUsers);
-
-        console.log("(servidor) Nuevo usuario agregado:", newUser);
-      } catch (error) {
-        console.error("(servidor)Error al agregar nuevo usuario:", error);
-        io.emit("server:updateusererror", {
-          message: "Error al agregar nuevo usuario.",
-        });
+      if (isPasswordUsed) {
+        console.log("No se agrego el usuario, contraseña repetida");
+        socket.emit("server:addusererror", "La contraseña ya está en uso.");
+      } else if (isAdminExists && userData.position === "Administrador") {
+        console.log("Ya existe otro administrador.");
+        socket.emit("server:addusererror", "Ya existe otro administrador.");
+      } else {
+        User.create(userData);
+        console.log("Usuario agregado");
+        io.emit("server:adduser", userData);
+        socket.broadcast.emit("server:useradded", userData); // Emite el evento a todos los clientes para informar que se agregó un usuario
       }
     });
 
@@ -110,9 +106,30 @@ export default (io) => {
       console.log(users);
     });
 
-    socket.on("client:deleteuser", async (id) => {
-      await User.findByIdAndDelete(id);
-      emitUsers();
+    socket.on("client:deleteuser", async (password) => {
+      try {
+        // Buscar al usuario por contraseña
+        const user = await User.findOne({ password });
+        console.log(password);
+
+        if (!user) {
+          console.log("no borrado");
+        } else {
+          // Eliminar el usuario
+          await User.deleteMany({ password: password });
+          console.log("Usuario borrado");
+
+          const users = await User.find();
+
+          io.emit("server:getusers", users); // Vuelve a emitir la lista de usuarios actualizada a todos los clientes
+        }
+      } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+        socket.emit(
+          "server:deleteusererror",
+          "Ocurrió un error al eliminar el usuario."
+        );
+      }
     });
   });
 };
